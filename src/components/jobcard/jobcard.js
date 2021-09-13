117e60fe8c40cb28bd8a95a33a2bd6c9ccdca4f2
gsalas31 <grace_salas94@hotmail.com>
2021-09-13 04:08:16 -0400

    jobs fetching

--- src/components/jobcard/jobcard.js
+++ src/components/jobcard/jobcard.js
@@ -1,9 +1,9 @@
-function JobCard (){
+function JobCard (props){
    return (
        <>
        <div className="jobcardoutline">
-       <h1>Job Title</h1>
-       <h3 id="companyname">Company's Name</h3>
+       <h1>{props.jobInfo.objective}</h1>
+       <h3 id="companyname">{props.jobInfo.organizations[0].name}</h3>
        <div className="positionlocation">
        <p>Position</p><p>Location</p>
        </div>
--- src/components/jobslist/jobslist.js
+++ src/components/jobslist/jobslist.js
@@ -1,13 +1,36 @@
+import { useEffect, useState } from "react"
 import JobCard from "../jobcard/jobcard"
 
+const URL= 'https://search.torre.co/opportunities/_search/?'
+
 function JobsList(){
-    let JobsArray=['Daniel', 'Alicia', 'Alice']
+    const [jobs, setJobs] = useState({});
+    const [fetching, setFetching] =  useState(true)
+
+    useEffect (()=>{
+        async function fetchJobs(){
+            fetch(URL, {
+                method: 'POST'
+            })
+            .then(res => res.json())
+            //.then(res => console.log(res))
+            .then(res => {
+                setJobs(res)
+                setFetching(false)
+            })
+        }
+        fetchJobs()
+    },[]
+    )
     return(
         <>
-       
+        {fetching == false ? (
+            jobs.results.map(job => (
+                //console.log(job)
+                <JobCard jobInfo = {job}/>
+             ) )
+        ) : 'fetching...'}
         </>
-
     )
 }
-
 export default JobsList
