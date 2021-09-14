c20100641e49bc81a33b644d76903a65cd69693b
gsalas31 <grace_salas94@hotmail.com>
2021-09-14 08:53:29 -0400

    search card layout

---
+++ src/components/searchcard/searchcard.js
@@ -0,0 +1,11 @@
+import './searchcard.css'
+
+function SearchCard (){
+    return(
+        <>
+            <input className='search_entry'></input>
+            <button className='search_button'>SEARCH</button>
+        </>
+    )
+}
+export default SearchCard
--- src/screen/mainscreen.js
+++ src/screen/mainscreen.js
@@ -1,16 +1,17 @@
 import { Link, Outlet } from 'react-router-dom';
+import SearchCard from '../components/searchcard/searchcard';
 import './mainscreen.css'
 function MainScreen(){
     return(
         <>
         <div className='container'>
-            <div className='search-container'></div>
+            <div className='search-container'><SearchCard/></div>
             <div className='main-container'>
-                <div className="main-container--filters">Filtrar</div>
+                <div className="main-container--filters"></div>
                 <div className='main-container--lists'>
                     <div className='buttons'>
-                        <Link to ='/jobs'><button className='buttonjobs'>Jobs</button></Link>
-                        <Link to ='/users'><button className='buttonusers'>Users</button></Link>
+                        <Link to ='/jobs'><button className='buttonjobs'>JOBS</button></Link>
+                        <Link to ='/users'><button className='buttonusers'>USERS</button></Link>
                     </div>
                     <Outlet/>
                 </div>
